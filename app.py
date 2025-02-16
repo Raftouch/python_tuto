d2d71ba11f79ed098d214bec902799eaf1e953fd
raftouch <ntimorshina@gmail.com>
2025-02-16 18:47:59 +0100

    refactor code - move logic to reusable function

--- app.py
+++ app.py
@@ -1,43 +1,47 @@
 import openpyxl as xl
 from openpyxl.chart import BarChart, Reference
 
-wb = xl.load_workbook('transactions.xlsx')
-sheet = wb['Sheet1']
-# cell = sheet['a1'] # column A, row 1
-cell = sheet.cell(1,1) # row 1, column 1
-print(cell.value)
-# print(sheet.max_row)
+def process_workbook(filename):
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet1']
+    # cell = sheet['a1'] # column A, row 1
+    cell = sheet.cell(1,1) # row 1, column 1
+    print(cell.value)
+    # print(sheet.max_row)
 
-for row in range(2, sheet.max_row + 1):
-    # print(raw)
-    cell = sheet.cell(row, 3)
-    # print(cell.value)
-    print(f"Row {row}, Value: {cell.value}, Type: {type(cell.value)}")
+    for row in range(2, sheet.max_row + 1):
+        # print(raw)
+        cell = sheet.cell(row, 3)
+        # print(cell.value)
+        print(f"Row {row}, Value: {cell.value}, Type: {type(cell.value)}")
 
-    if isinstance(cell.value, str):
-        try:
-            cell.value = float(cell.value)
-        except ValueError:
+        if isinstance(cell.value, str):
+            try:
+                cell.value = float(cell.value)
+            except ValueError:
+                print(f'Skip non-numeric value in row {row}: {cell.value}')
+
+        if isinstance(cell.value, (int, float)):
+            corrected_price = cell.value * 0.9
+            # print(corrected_price)
+            corrected_price_cell = sheet.cell(row, 4)
+            corrected_price_cell.value = corrected_price
+        else:
             print(f'Skip non-numeric value in row {row}: {cell.value}')
 
-    if isinstance(cell.value, (int, float)):
-        corrected_price = cell.value * 0.9
-        # print(corrected_price)
-        corrected_price_cell = sheet.cell(row, 4)
-        corrected_price_cell.value = corrected_price
-    else:
-        print(f'Skip non-numeric value in row {row}: {cell.value}')
+    values = Reference(sheet, 
+                    min_row=2, 
+                    max_row=sheet.max_row, 
+                    min_col=4, 
+                    max_col=4)
 
-values = Reference(sheet, 
-                   min_row=2, 
-                   max_row=sheet.max_row, 
-                   min_col=4, 
-                   max_col=4)
+    chart = BarChart()
+    chart.add_data(values)
 
-chart = BarChart()
-chart.add_data(values)
+    sheet.add_chart(chart, 'e2')
 
-sheet.add_chart(chart, 'e2')
+    wb.save(filename)
 
-wb.save('transactions_with_corrected_ptice.xlsx')
+
+process_workbook('transactions.xlsx')
 
